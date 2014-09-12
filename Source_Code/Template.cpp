780f886df34cd219c61f7204a50a675ab025974e
Rijul1204 <rashedulhasanrijul@gmail.com>
2014-09-12 10:01:34 +0600

    Default Template

---
+++ Source_Code/Template.cpp
@@ -0,0 +1,58 @@
+/*
+Author      : Rashedul Hasan Rijul ( Silent_coder ).
+Created on  : 2014-09-12
+*/
+
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+#include<stdlib.h>
+#include<ctype.h>
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+#include<queue>
+#include<stack>
+#include<map>
+#include<set>
+using namespace std;
+
+#define maxm 2010
+#define inf (1<<29)
+#define ii int
+
+#define pi  acos(-1.0)
+#define eps 1e-9
+#define iseq(a,b) (fabs(a-b)<eps)
+
+#define pii pair<int,int>
+#define mp  make_pair
+#define uu first
+#define vv second
+
+ii on(ii n,ii k){ return (n|(1<<k)); }
+ii off(ii n,ii k){ return (n-(n&(1<<k))); }
+bool chck(ii n,ii k){ return (n&(1<<k)); }
+
+ii mini(ii a,ii b){ if(a<b) return a;  return b;  }
+ii maxi(ii a,ii b){ if(a>b) return a;  return b;  }
+
+int n,m;
+
+int main(){
+
+    int i,j,k,l,test,t=1;
+
+    //freopen("in.txt","r",stdin);
+    //freopen("out.txt","w",stdout);
+
+    scanf("%d",&test);
+
+    while(test--){
+
+    }
+
+    return 0;
+}
+
